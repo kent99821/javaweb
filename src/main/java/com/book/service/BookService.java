dc934f76b35694f83801830b3c9f6152b6342178
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-06-06 21:59:26 +0800

    新建BookService

---
+++ src/main/java/com/book/service/BookService.java
@@ -0,0 +1,19 @@
+package com.book.service;
+
+import com.book.pojo.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    public void addBook(Book book);
+
+    public void deleteBookById(Integer id);
+
+    public void updateBook(Book book);
+
+    public Book queryBookById(Integer id);
+
+    public List<Book> queryBooks();
+
+}
---
+++ src/main/java/com/book/service/impl/BookServiceImpl.java
@@ -0,0 +1,38 @@
+package com.book.service.impl;
+
+import com.book.dao.BookDao;
+import com.book.dao.impl.BookDaoImpl;
+import com.book.pojo.Book;
+import com.book.service.BookService;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    private BookDao bookDao = new BookDaoImpl();
+
+    @Override
+    public void addBook(Book book) {
+        bookDao.addBook(book);
+    }
+
+    @Override
+    public void deleteBookById(Integer id) {
+        bookDao.deleteBookById(id);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        bookDao.updateBook(book);
+    }
+
+    @Override
+    public Book queryBookById(Integer id) {
+        return bookDao.queryBookById(id);
+    }
+
+    @Override
+    public List<Book> queryBooks() {
+        return bookDao.queryBooks();
+    }
+}
